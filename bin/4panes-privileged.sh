7446ab36b7a9c6313ac46eaaceaed399f80423a3
Jonathan Springer <jonpspri@gmail.com>
2020-01-10 10:53:28 -0500

    Added swapfile management to ansible

--- bin/4panes-privileged.sh
+++ bin/4panes-privileged.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-tmux new-session ssh -i ~/.ssh/openwhisk-key.pem root@x86-64.multiarch-demo.com \; \
+tmux new-session ssh -i ~/.ssh/openwhisk-key.pem root@x86-64-a.multiarch-demo.com \; \
  split-window -h ssh -i ~/.ssh/openwhisk-key.pem ubuntu@ppc64le.multiarch-demo.com \; \
  split-window -t "%0" -v ssh -i ~/.ssh/openwhisk-key.pem ubuntu@s390x.multiarch-demo.com \; \
- split-window -t "%1" -v ssh -i ~/.ssh/openwhisk-key.pem root@aarch64.multiarch-demo.com \; \
+ split-window -t "%1" -v ssh -i ~/.ssh/openwhisk-key.pem root@aarch64-a.multiarch-demo.com \; \
  setw synchronize-panes on \;
