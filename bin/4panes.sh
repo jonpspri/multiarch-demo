73f077358154ce0d7d5d1cffebf2df379ad51251
Jonathan Springer <jonpspri@gmail.com>
2020-01-31 10:02:05 +0000

    Rationalize demo

---
+++ bin/4panes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+#  This file is set up for the demo environment originally shown at IBM's
+#  Fast Start 2020 conference.  Unless you're in magical posession of the
+#  SSH login keys, it WILL NOT work for you without modification.
+#
+#  However, if you've never encountered the magic of tmux, it's worth further exploration.
+#
+
+{
+    read -r machine1 user1
+    read -r machine2 user2
+    read -r machine3 user3
+    read -r machine4 user4
+} <<< "$("$(dirname "$0")"/echo-4slaves.sh "$@")"
+
+tmux new-session ssh -i ~/.ssh/openwhisk-key.pem $user1@$machine1 \; \
+ split-window -h ssh -i ~/.ssh/openwhisk-key.pem $user2@$machine2 \; \
+ split-window -t "%0" -v ssh -i ~/.ssh/openwhisk-key.pem $user3@$machine3 \; \
+ split-window -t "%1" -v ssh -i ~/.ssh/openwhisk-key.pem $user4@$machine4 \; \
+ setw synchronize-panes on \;
--- bin/echo-4slaves.sh
+++ bin/echo-4slaves.sh
@@ -1,13 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+if [ "$(getopt -T)" == " --" ]; then
+  echo "Not using the appropriate getopt.  Please obtain 'GNU getout'"; exit 255
+fi
+eval set -- "$(getopt -op --longoptions privileged: -n 'getopt' -- "$@")"
+
+privileged=no
+while true; do
+  case "$1" in
+    -p | --privileged) privileged=yes; shift;;
+    -- ) shift; break;;
+    * ) break;;
+  esac
+done
 
 hostpath="$(dirname "$(realpath $0)")/../ansible"
-(
+{
     while IFS= read -r line; do
         [ "$line" == '[slaves]' ] && break
     done
     #shellcheck disable=SC2034
     for i in $(seq 1 4); do
-        IFS=' ' read machine foo
-        echo $machine
+        IFS=' ' read machine assignments
+        [ "$privileged" == 'no' ] && { echo $machine demo; continue; }
+        username=root
+        for assignment in $assignments; do
+          variable=${assignment%%=*}
+          setting=${assignment#*=}
+          [ "$variable" == 'ansible_user' ] && username="$setting"
+        done
+        echo $machine $username
     done
-) < "$hostpath/hosts"
+} < "$hostpath/hosts"
